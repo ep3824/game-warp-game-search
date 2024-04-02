f77f77988c277d08ed94e6c4595d355d66ca4987
Ethan Parent <ethan.parent1@gmail.com>
2024-04-02 16:21:57 -0500

    Fix Spring CORS issues

--- server/demo/src/main/java/com/example/demo/SimpleApiController.java
+++ server/demo/src/main/java/com/example/demo/SimpleApiController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "http://192.168.70.11:3000")
 public class SimpleApiController {
 
     @Value("${RAWG_API_KEY}")
@@ -18,8 +22,7 @@ public class SimpleApiController {
     @GetMapping("/api/{endpoint}")
     public Mono<String> fetchFromRawgApi(@PathVariable String endpoint) {
         return webClient.get()
-                .uri("/{endpoint}?key={apiKey}", endpoint, apiKey)
-                .retrieve()
+                .uri("/{endpoint}?key={apiKey}", Map.of("endpoint", endpoint, "apiKey", apiKey))                .retrieve()
                 .bodyToMono(String.class)
                 .onErrorResume(e -> Mono.just("Failed to fetch data: " + e.getMessage()));
     }
